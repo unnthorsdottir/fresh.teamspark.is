60cc6c9140f6e09b3deb6e2faef375e706760d2e
Katla María Unnþórsdóttir <kmunnthors@pm.me>
2026-09-16 17:38:13 +0000

    First steps

---
+++ spark-header.js
@@ -0,0 +1,18 @@
+class MyHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <header>
+        <img src="IMG_P755.png" class="title"></img>
+        <nav class="main-nav">
+			<a href="#"><h1>Bílar</h1></a>
+			<a href="#"><h1>Styrktaraðilar</h1></a>
+			<a href="#"><h1>Liðsmenn</h1></a>
+			<a href="#"><h1>Keppnir</h1></a>
+			<a href="#"><h1>Fréttir</h1></a>
+			<a href="#"><h1>Sækja um</h1></a>
+		</nav>
+      </header>
+    `;
+  }
+}
+customElements.define('spark-header', MyHeader);
